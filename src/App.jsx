b73030439c741ce5dfbf96ddfd77f26da5a94be9
liansun96 <toetetaung3296@gmail.com>
2023-08-03 13:13:19 +0630

    lian commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Test from './components/Test'
 
 const App = () => {
   return (
-    <div className='text-8xl font-bold text-red-600'>App</div>
+    <div className='text-8xl font-bold text-red-600'>
+      <Test/>
+    </div>
   )
 }
 
